283daed4ccd0863c32fb4b92237add7cca8e4ab8
foglerori <foglerori@gmail.com>
2018-08-31 09:47:36 +0300

    cool commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Game from './game.js';
+
+// render game element to root
+ReactDOM.render(<Game />, document.getElementById('root'));
